0aca742408f2428ae27944534dda056a4bb40d87
Akira <adr.y.4@hotmail.com>
2018-10-02 19:05:00 +0200

    first commit

--- Geolocal/src/pages/navegar/navegar.ts
+++ Geolocal/src/pages/navegar/navegar.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { GESTURE_PRIORITY_TOGGLE } from 'ionic-angular/umd/gestures/gesture-controller';
 
+declare var google: any;
 
 @Component({
   selector: 'page-navegar',
@@ -8,9 +10,33 @@ import { NavController } from 'ionic-angular';
 })
 export class NavegarPage {
 
-  constructor(public navCtrl: NavController) {
+  @ViewChild ('map') mapRef: ElementRef;
 
+
+  constructor(public navCtrl: NavController) {  }
+
+  ionViewDidLoad(){
+      this.showMap();
   }
+  showMap(){
+    const location = new google.maps.LatLng(51.507351, -0.127758);
   
+    const options = {
+    center: location,
+    zoom: 15,
+    streetViewControl: false,
+    mapTypeId: 'roadmap'
+    }
+
+    const map= new google.maps.Map(this.mapRef.nativeElement, options);
+    this.addMarker(location, map);
+  }
+
+    addMarker(position, map){
+      return new google.maps.Marker({
+        position,
+        map
+      });
+    }
   }
 
